6d48e03a6bb6b40ad9d3fefc02f73c26912b9ade
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-21 10:31:51 -0400

    Updated activty add layout with due date toggle, date picker and buttons

--- app/src/main/java/ca/georgiancollege/todoit/AddTaskActivity.kt
+++ app/src/main/java/ca/georgiancollege/todoit/AddTaskActivity.kt
@@ -1,14 +1,22 @@
 package ca.georgiancollege.todoit
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import ca.georgiancollege.todoit.databinding.ActivityAddTaskBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddTaskActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddTaskBinding
+    private var calendar = Calendar.getInstance()
+    private val context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,5 +47,49 @@ class AddTaskActivity : AppCompatActivity() {
         binding.menuBar.userProfileButton.setOnClickListener {
             Log.d("MenuBar", "User profile button clicked")
         }
+
+        binding.cancelButton.setOnClickListener {
+            Log.d("CancelButton", "Cancel button clicked")
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        binding.saveButton.setOnClickListener {
+            Log.d("SaveButton", "Save button clicked")
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        binding.selectDateButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        binding.selectedDateLinearLayout.visibility = View.GONE
+
+        binding.dueDateToggleSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.selectedDateLinearLayout.visibility = View.VISIBLE
+            } else {
+                binding.selectedDateLinearLayout.visibility = View.GONE
+                binding.selectedDateLabelTextView.text = context.getString(R.string.select_a_date_label_text)
+            }
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePickerDialog = DatePickerDialog(this, {_,year: Int, month: Int, day: Int ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, day)
+            val dateFormat = SimpleDateFormat("MM/dd/yyy", Locale.getDefault())
+            val formattedDate = dateFormat.format(selectedDate.time)
+            binding.selectedDateLabelTextView.text = formattedDate
+        },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 }
